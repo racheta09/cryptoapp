2522eb07cc3e794bfe26ce8755a96910e9236f4a
racheta <surajshogun@gmail.com>
2022-02-26 18:34:36 +0530

    Currencies & News

---
+++ src/components/LineChart.jsx
@@ -0,0 +1,92 @@
+import React from "react"
+import { Line } from "react-chartjs-2"
+import { Col, Row, Typography } from "antd"
+import moment from "moment"
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title as ChartTitle,
+  Tooltip,
+  Legend,
+} from "chart.js"
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  ChartTitle,
+  Tooltip,
+  Legend
+)
+
+const { Title } = Typography
+
+const LineChart = ({ coinHistory, currentPrice, coinName }) => {
+  const coinPrice = []
+  const coinTimestamp = []
+
+  for (let i = 0; i < coinHistory?.data?.history?.length; i++) {
+    coinPrice.push(coinHistory?.data?.history[i].price)
+    coinTimestamp.push(
+      new Date(moment.unix(coinHistory?.data?.history[i].timestamp)).toLocaleDateString()
+    )
+  }
+
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: "Price In USD",
+        data: coinPrice,
+        fill: false,
+        backgroundColor: "#0071bd",
+        borderColor: "#0071bd",
+      },
+    ],
+  }
+
+  // const options = {
+  //     scales: {
+  //       yAxes: [
+  //         {
+  //           ticks: {
+  //             beginAtZero: true,
+  //           },
+  //         },
+  //       ],
+  //     },
+  // }
+
+  const options = {
+    scales: {
+      y: {
+        min: 0,
+      },
+    },
+  }
+
+  return (
+    <>
+      <Row className="chart-header">
+        <Title level={2} className="chart-title">
+          {coinName} Price Chart{" "}
+        </Title>
+        <Col className="price-container">
+          <Title level={5} className="price-change">
+            Change: {coinHistory?.data?.change}%
+          </Title>
+          <Title level={5} className="current-price">
+            Current {coinName} Price: $ {currentPrice}
+          </Title>
+        </Col>
+      </Row>
+      <Line data={data} options={options} />
+    </>
+  )
+}
+
+export default LineChart
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,50 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "148051b681msh814554a9707ef22p10c9a3jsnb41a8a23188e",
+}
+
+const baseUrl = "https://coinranking1.p.rapidapi.com"
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timeperiod }) =>
+        createRequest(`/coin/${coinId}/history?timePeriod=${timeperiod}`),
+    }),
+  }),
+})
+
+export const {
+  useGetCryptosQuery,
+  useGetCryptoDetailsQuery,
+  useGetCryptoHistoryQuery,
+} = cryptoApi
+
+// var options = {
+//     method: 'GET',
+//     ,
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       timePeriod: '24h',
+//       tiers: '1',
+//       orderBy: 'marketCap',
+//       orderDirection: 'desc',
+//       limit: '50',
+//       offset: '0'
+//     },
+
+//   };
