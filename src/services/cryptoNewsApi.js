f13bdd47d9e4a003ef1912b0e3813a01f42e949e
racheta09 <surajshogun@gmail.com>
2022-02-26 18:56:53 +0530

    Update cryptoNewsApi.js

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 const cryptoNewsHeaders = {
   "x-bingapis-sdk": "true",
   "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
-  "x-rapidapi-key": "148051b681msh814554a9707ef22p10c9a3jsnb41a8a23188e",
+  "x-rapidapi-key": "",
 }
 
 const baseUrl = "https://bing-news-search1.p.rapidapi.com"
